a8cb5102dec92cdcb1d022e74910327f604e503a
Daniel Valcarce <davamix@gmail.com>
2020-03-27 04:12:20 +0100

    Grab the image and send it to the backend

--- app/src/controllers/validate_controller.py
+++ app/src/controllers/validate_controller.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from flask import request
 from flask.views import MethodView
+import base64
 
 class ValidateController(MethodView):
     def __init__(self):
@@ -13,7 +14,12 @@ class ValidateController(MethodView):
         data = request.get_json()
 
         if data:
-            print(f"DATA: {data}")
+            content = base64.b64decode(data)
+            
+            # Only for testing purposes. The image is saved in the container
+            with open("image.png", "wb") as f:
+                f.write(content)
+
         else:
             print("NO DATA")
 
